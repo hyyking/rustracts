26f99115ef0321182410daf0e6b6e3bc85977ee7
hyyking <leoduret@outlook.com>
2019-10-30 15:11:40 +0100

    feat(lib): Contract context and move futurescontract in it's module

---
+++ src/futures.rs
@@ -0,0 +1,171 @@
+use futures::{
+    future::Future,
+    task::{Context, Poll},
+};
+
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
+
+use crate::{Contract, ContractContext, ContractExt, Status};
+
+#[must_use = "contracts do nothing unless polled or awaited"]
+pub struct FuturesContract<F, C, R>
+where
+    C: ContractContext,
+    F: FnOnce(C) -> R + Copy,
+{
+    creation: Instant,
+    expire: Duration,
+    context: Arc<Mutex<C>>,
+    on_exe: F,
+}
+
+impl<F, C, R> FuturesContract<F, C, R>
+where
+    C: ContractContext,
+    F: FnOnce(C) -> R + Copy,
+{
+    pub fn new(expire: Duration, context: C, on_exe: F) -> Self {
+        Self {
+            creation: Instant::now(),
+            expire,
+            context: Arc::new(Mutex::new(context)),
+            on_exe,
+        }
+    }
+}
+
+impl<F, C, R> Contract for FuturesContract<F, C, R>
+where
+    C: ContractContext,
+    F: FnOnce(C) -> R + Copy,
+{
+    type Output = R;
+
+    fn is_valid(&self) -> bool {
+        (*self.context.lock().unwrap()).is_valid()
+    }
+
+    fn is_expired(&self) -> bool {
+        Instant::now().duration_since(self.creation) > self.expire
+    }
+
+    fn execute(&self) -> Status<Self::Output> {
+        let context = self.context.lock().unwrap();
+        Status::Completed((self.on_exe)(*context))
+    }
+
+    fn void(&self) -> Status<Self::Output> {
+        Status::Voided
+    }
+}
+
+impl<F, C, R> ContractExt<C> for FuturesContract<F, C, R>
+where
+    C: ContractContext,
+    F: FnOnce(C) -> R + Copy,
+{
+    fn get_context(&self) -> Arc<Mutex<C>> {
+        self.context.clone()
+    }
+}
+
+impl<F, C, R> Future for FuturesContract<F, C, R>
+where
+    C: ContractContext,
+    F: FnOnce(C) -> R + Copy,
+{
+    type Output = Status<<Self as Contract>::Output>;
+
+    fn poll(self: std::pin::Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        let mv = (self.is_expired(), self.is_valid());
+
+        // wakes up 4 times during it's lifetime to check if it should be voided
+        std::thread::sleep(self.expire / 4);
+        cx.waker().clone().wake();
+
+        match mv {
+            (true, true) => Poll::Ready(self.execute()),
+            (false, true) => Poll::Pending,
+            (_, false) => Poll::Ready(self.void()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::FuturesContract;
+    use crate::{ContractContext, ContractExt, Status};
+
+    use std::time::Duration;
+
+    #[derive(Clone, Copy)]
+    struct CustomContext(usize);
+    impl ContractContext for CustomContext {
+        // Contract is valid under condition
+        fn is_valid(&self) -> bool {
+            self.0 > 2
+        }
+    }
+
+    impl ContractContext for usize {} // Contract is always valid
+
+    #[test]
+    fn simple_contract() {
+        let context: usize = 3;
+        let c = FuturesContract::new(Duration::from_secs(1), context, |con| -> usize { con + 5 });
+
+        if let Status::Completed(value) = futures::executor::block_on(c) {
+            assert_eq!(value, 8)
+        } else {
+            assert!(false)
+        }
+    }
+
+    #[test]
+    fn voided_contract() {
+        let context = CustomContext(3);
+
+        let c = FuturesContract::new(Duration::from_secs(10), context, |con| -> usize {
+            con.0 + 5
+        });
+
+        let handle = std::thread::spawn({
+            let mcontext = c.get_context();
+            move || {
+                (*mcontext.lock().unwrap()).0 = 1;
+            }
+        });
+
+        if let Status::Completed(val) = futures::executor::block_on(c) {
+            assert_ne!(val, 1);
+        } else {
+            assert!(true); // Contract should be voided because updated value is 1 which is < 2
+        }
+
+        handle.join().unwrap();
+    }
+
+    #[test]
+    fn updated_contract() {
+        let context = CustomContext(3);
+        let c = FuturesContract::new(Duration::from_secs(1), context, |con| -> usize {
+            con.0 + 5
+        });
+
+        let handle = std::thread::spawn({
+            let mcontext = c.get_context();
+            move || {
+                (*mcontext.lock().unwrap()).0 += 2;
+            }
+        });
+
+        if let Status::Completed(value) = futures::executor::block_on(c) {
+            assert_eq!(value, 10);
+        } else {
+            assert!(false);
+        }
+
+        handle.join().unwrap();
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,24 @@
-use futures::{
-    future::Future,
-    task::{Context, Poll},
-};
 use std::sync::{Arc, Mutex};
-use std::time::{Duration, Instant};
 
-pub trait ValidContract {
+pub trait ContractContext: Copy {
     fn is_valid(&self) -> bool {
         true
     }
 }
 
-pub trait Contract: ValidContract + Sized {
+pub trait Contract: Sized {
     type Output;
 
-    fn is_met(&self) -> bool;
-    fn execute(&self) -> Self::Output;
-    fn void(self) {}
+    fn is_valid(&self) -> bool {
+        true
+    }
+    fn is_expired(&self) -> bool;
+    fn execute(&self) -> Status<Self::Output>;
+    fn void(&self) -> Status<Self::Output>;
+}
+
+pub trait ContractExt<C> {
+    fn get_context(&self) -> Arc<Mutex<C>>;
 }
 
 pub enum Status<R> {
@@ -24,138 +26,6 @@ pub enum Status<R> {
     Voided,
 }
 
-#[must_use = "contracts do nothing unless polled or awaited"]
-pub struct FuturesContract<F, C, R>
-where
-    C: Copy + Send,
-    F: FnOnce(C) -> R + Copy,
-{
-    creation: Instant,
-    expire: Duration,
-    context: Arc<Mutex<C>>,
-    on_exe: F,
-}
+mod futures;
 
-impl<F, C, R> FuturesContract<F, C, R>
-where
-    C: Copy + Send,
-    F: FnOnce(C) -> R + Copy,
-{
-    pub fn new(expire: Duration, context: Arc<Mutex<C>>, on_exe: F) -> Self {
-        Self {
-            creation: Instant::now(),
-            expire,
-            context,
-            on_exe,
-        }
-    }
-}
-
-// TODO: Find a better way to validate contracts in a general way
-impl<F, R> ValidContract for FuturesContract<F, usize, R>
-where
-    F: FnOnce(usize) -> R + Copy,
-{
-    fn is_valid(&self) -> bool {
-        *self.context.lock().unwrap() > 2
-    }
-}
-
-impl<F, R> Contract for FuturesContract<F, usize, R>
-where
-    F: FnOnce(usize) -> R + Copy,
-{
-    type Output = R;
-
-    fn is_met(&self) -> bool {
-        Instant::now().duration_since(self.creation) > self.expire
-    }
-
-    fn execute(&self) -> Self::Output {
-        let context = self.context.lock().unwrap();
-        (self.on_exe)(*context)
-    }
-}
-
-impl<F, R> Future for FuturesContract<F, usize, R>
-where
-    F: FnOnce(usize) -> R + Copy,
-{
-    type Output = Status<<Self as Contract>::Output>;
-
-    fn poll(self: std::pin::Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-        let mv = (self.is_met(), self.is_valid());
-
-        // wakes up 4 times during it's lifetime to check if it should be voided
-        std::thread::sleep(self.expire / 4);
-        cx.waker().clone().wake();
-
-        match mv {
-            (true, true) => Poll::Ready(Status::Completed(self.execute())),
-            (false, true) => Poll::Pending,
-            (_, false) => Poll::Ready(Status::Voided),
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::{FuturesContract, Status};
-    use std::sync::{Arc, Mutex};
-    use std::time::Duration;
-
-    #[test]
-    fn simple_contract() {
-        let context = Arc::new(Mutex::new(3));
-        let c = FuturesContract::new(Duration::from_secs(1), context, |con| -> usize { con + 5 });
-        if let Status::Completed(value) = futures::executor::block_on(c) {
-            assert_eq!(value, 8)
-        } else {
-            assert!(false)
-        }
-    }
-
-    #[test]
-    fn voided_contract() {
-        let context = Arc::new(Mutex::new(3 as usize));
-
-        let handle = std::thread::spawn({
-            let mcontext = context.clone();
-            move || {
-                *mcontext.lock().unwrap() = 1;
-            }
-        });
-        // Contract will be voided since 1 < 2
-        let c = FuturesContract::new(Duration::from_secs(4), context, |con| -> usize { con + 5 });
-
-        if let Status::Completed(_) = futures::executor::block_on(c) {
-            assert!(false);
-        } else {
-            assert!(true);
-        }
-
-        handle.join().unwrap();
-    }
-
-    #[test]
-    fn updated_contract() {
-        let context = Arc::new(Mutex::new(3 as usize));
-
-        let handle = std::thread::spawn({
-            let mcontext = context.clone();
-            move || {
-                *mcontext.lock().unwrap() += 2;
-            }
-        });
-        // Contract will be voided since 1 < 2
-        let c = FuturesContract::new(Duration::from_secs(1), context, |con| -> usize { con + 5 });
-
-        if let Status::Completed(value) = futures::executor::block_on(c) {
-            assert_eq!(value, 10);
-        } else {
-            assert!(true);
-        }
-
-        handle.join().unwrap();
-    }
-}
+pub use crate::futures::FuturesContract;
